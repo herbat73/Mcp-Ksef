4b8912c44949b524bf3b58e68ff01e41e20c682e
Adam Adamczyk <aadamczyk@figaro.com.pl>
2026-01-08 21:14:30 +0100

    get invoice by ksef

---
+++ Models/CompanyInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace RemoteMcpKsef.Models;
+
+public class CompanyInfo
+{
+    public string VatId { get; set; }
+    public string Name { get; set; }
+    public string AdresLine1 { get; set; }
+    public string AdresLine2 { get; set; }
+    public string CountryCode { get; set; }
+}
--- Tools/KsefTools.cs
+++ Tools/KsefTools.cs
@@ -1,5 +1,12 @@
 ﻿using System.ComponentModel;
+using KSeF.Client.Api.Services;
+using KSeF.Client.Clients;
+using KSeF.Client.Core.Interfaces;
+using KSeF.Client.Core.Interfaces.Clients;
+using KSeF.Client.Core.Interfaces.Services;
+using KSeF.Client.Core.Models.Authorization;
 using ModelContextProtocol.Server;
+using RemoteMcpKsef.Models;
 
 namespace RemoteMcpKsef.Tools;
 
@@ -12,17 +19,53 @@ public class KsefTools
 {
     private readonly ILogger<KsefTools> _logger;
     private readonly string _toolName = "KSeF Tools";
+    private IKSeFClient _ksefClient;
+    private readonly IAuthorizationClient _authorizationClient;
+    private readonly ICryptographyService _cryptographyService;
+    private readonly IConfiguration _configuration;
+    private string? _accessToken = null;
+    private string? _openSessionReferenceNumber =  null;
     
-    public KsefTools(ILogger<KsefTools> logger)
+    public KsefTools(ILogger<KsefTools> logger, IKSeFClient ksefClient, IAuthorizationClient authorizationClient, ICryptographyService cryptographyService, IConfiguration configuration)
     {
         _logger = logger;
+        _ksefClient =  ksefClient;
+        _authorizationClient = authorizationClient;
+        _cryptographyService = cryptographyService;
+        _configuration = configuration;
     }
     
-    [McpServerTool, Description("Pobierz fakturę")]
-    public string GetInvoice([Description("Numer faktury")] string invoiceNumber)
+    [McpServerTool, Description("Pobranie faktury po numerze referencyjnym")]
+    public async Task<string> GetInvoice([Description("Numer referencyjny ksef")] string ksefReferenceNumber)
     {
-        _logger.LogInformation($"{_toolName}.{nameof(GetInvoice)} called invoiceNumber: {invoiceNumber}");
+        _logger.LogInformation($"{_toolName}.{nameof(GetInvoice)} called invoiceNumber: {ksefReferenceNumber}");
+        var sellerSettings = _configuration.GetSection("SellerSettings").Get<CompanyInfo>();
+        var ksefSettings = _configuration.GetSection(nameof(KsefSettings)).Get<KsefSettings>();
+
+        _accessToken ??= await GetAccessTokenAsync(sellerSettings.VatId, ksefSettings.Token);
         
-        return new string(invoiceNumber.Reverse().ToArray());
+        var invoice = await _ksefClient.GetInvoiceAsync(ksefReferenceNumber, _accessToken);
+        return invoice;
+    }
+    
+    private async Task<string> GetAccessTokenAsync(string nip, string ksefToken)
+    {
+        _logger.LogInformation($"GetAccessTokenAsync for nip : {nip}");
+        
+        const AuthenticationTokenContextIdentifierType contextType = AuthenticationTokenContextIdentifierType.Nip;
+        AuthenticationTokenAuthorizationPolicy? authorizationPolicy = null;
+        var authCoordinator = new AuthCoordinator(_authorizationClient);
+            
+        var result = await authCoordinator.AuthKsefTokenAsync(
+            contextType,
+            nip,
+            ksefToken,
+            _cryptographyService,
+            EncryptionMethodEnum.Rsa,
+            authorizationPolicy,
+            CancellationToken.None
+        );
+            
+        return result.AccessToken.Token;
     }
 }
