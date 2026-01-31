0ffe348ab1c21a910355858c0f0857d12dfbdcc3
Adam Adamczyk <aadamczyk@figaro.com.pl>
2026-01-31 20:55:08 +0100

    commit work

--- Mcp-Ksef.HybridApp/Tools/IKsefTools.cs
+++ Mcp-Ksef.HybridApp/Tools/IKsefTools.cs
@@ -1,6 +1,8 @@
-﻿namespace McpKsef.HybridApp.Tools;
+﻿using KSeF.Client.Core.Models.Invoices;
+
+namespace McpKsef.HybridApp.Tools;
 
 public interface IKsefTools
 {
-    Task<string> GetInvoice(string ksefReferenceNumber);
+    Task<string> GetInvoice(string ksefNumber);
 }
--- Mcp-Ksef.HybridApp/Tools/KsefTools.cs
+++ Mcp-Ksef.HybridApp/Tools/KsefTools.cs
@@ -4,13 +4,14 @@ using KSeF.Client.Core.Interfaces;
 using KSeF.Client.Core.Interfaces.Clients;
 using KSeF.Client.Core.Interfaces.Services;
 using KSeF.Client.Core.Models.Authorization;
+using KSeF.Client.Core.Models.Invoices;
 using Shared.Consts;
 using ModelContextProtocol.Server;
 
 namespace McpKsef.HybridApp.Tools;
 
 [McpServerToolType]
-public class KsefTools
+public class KsefTools : IKsefTools
 {
     private readonly ILogger<KsefTools> _logger;
     private readonly string? _ksefToken;
@@ -18,27 +19,50 @@ public class KsefTools
     private static string? _authToken;
     private readonly IAuthorizationClient _authorizationClient;
     private readonly ICryptographyService _cryptographyService;
+    private readonly IKSeFClient _ksefClient;
     
-    //public KsefTools(KsefAppSettings ksefAppSettings, ILogger<KsefTools> logger)
-    public KsefTools(ILogger<KsefTools> logger, IAuthorizationClient authorizationClient, ICryptographyService cryptographyService)
+    public KsefTools(ILogger<KsefTools> logger, IAuthorizationClient authorizationClient, ICryptographyService cryptographyService, IKSeFClient ksefClient)
     {
         _logger = logger;
         _ksefToken = Environment.GetEnvironmentVariable(EnvironmentConsts.KsefToken);
         _vatId = Environment.GetEnvironmentVariable(EnvironmentConsts.VatId);
         _authorizationClient =  authorizationClient;
         _cryptographyService =  cryptographyService;
+        _ksefClient = ksefClient;
     }
     
-    [McpServerTool(Name = "get_invoice_by_reference", Title = "Pobierz fakturę po numerze referencyjnym")]
-    [Description("Pobiera fakturę po numerze referencyjnym")]
-    public async Task<string> GetInvoice([Description("Numer referencyjny ksef")] string ksefReferenceNumber)
+    [McpServerTool(Name = "get_invoice_by_ksef", Title = "Pobierz fakturę po numerze referencyjnym ksef")]
+    [Description("Pobiera fakturę po numerze referencyjnym ksef")]
+    public async Task<string> GetInvoice([Description("Numer referencyjnym ksef")] string ksefReferenceNumber)
     {
-        _logger.LogInformation($"{AppConsts.KsefToolName}.{nameof(GetInvoice)} called invoiceNumber: {ksefReferenceNumber}");
+        _logger.LogInformation($"{AppConsts.KsefToolName}.{nameof(GetInvoice)} called ksefReferenceNumber: {ksefReferenceNumber}");
         await VerifyAuthToken();
         
-        var invoice = $"yo {ksefReferenceNumber} ksefToken {_ksefToken}";
+        var invoice = await _ksefClient.GetInvoiceAsync(ksefReferenceNumber, _authToken);
+        
         return invoice;
     }
+    
+    // private async Task<InvoiceSummary> GetInvoiceSummary(string ksefReferenceNumber, DateTime invoiceDate)
+    // {
+    //     var invoiceMetadataQueryRequest = new InvoiceQueryFilters
+    //     {
+    //         KsefNumber = ksefReferenceNumber,
+    //         DateRange = new DateRange
+    //         {
+    //             From = invoiceDate.AddMinutes(-10),
+    //             To = invoiceDate.AddMinutes(10),
+    //             DateType = DateType.Issue
+    //         }
+    //     };
+    //         
+    //     var metadata = await _ksefClient.QueryInvoiceMetadataAsync(
+    //         requestPayload: invoiceMetadataQueryRequest,
+    //         accessToken: _authToken);
+    //     
+    //     var invoiceMetadata = metadata.Invoices.Single(x => x.KsefNumber == ksefReferenceNumber);
+    //     return invoiceMetadata;
+    // }
     
     private async Task<string> GetAccessTokenAsync(string nip, string ksefToken)
     {
@@ -57,7 +81,9 @@ public class KsefTools
             authorizationPolicy,
             CancellationToken.None
         );
-            
+        
+        _logger.LogInformation($"{AppConsts.KsefToolName}.{nameof(GetAccessTokenAsync)} return AccessToken Length: {result.AccessToken.Token.Length}");
+        
         return result.AccessToken.Token;
     }
 
